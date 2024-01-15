40242c66d781712141e411faf6829eb5c3740dec
Ariel Pellegrino <ariel.pell@gmail.com>
2024-01-15 20:52:18 -0300

    update: arquivos pokedex

--- moduleJavascript/aulas/pokedex/assets/javascript/main.js
+++ moduleJavascript/aulas/pokedex/assets/javascript/main.js
@@ -1,12 +1,14 @@
 const offset = 0;
-const limit =  10; 
+const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url).then(function(response){
-   response.json().then()
-}).catch(function (err) {
-    console.log(err)
-}).finally(function() {
-    console.log('Request completed')
-})
+fetch(url)
+    .then(response => response.json())
+    .then(jsonBody => jsonBody.results)
+    .then((pokemons) => {
+            debugger
+        console.log(pokemonList)
+    })
+    .catch((error) => console.log(error))
+
