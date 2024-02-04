4525cf3ab827e2e1a468446fea15865f3eeda66f
Ariel Pellegrino <ariel.pell@gmail.com>
2024-02-04 18:51:20 -0300

    update: typescripp course

---
+++ modulo.angular/TypeScript/src/index.js
@@ -0,0 +1,7 @@
+function printaObjeto(pessoa) {
+    console.log(pessoa);
+}
+console.log(printaObjeto({
+    name: "bruce wayner",
+    vulgo: "Batman",
+}));
--- modulo.angular/TypeScript/src/index.ts
+++ modulo.angular/TypeScript/src/index.ts
@@ -1,13 +1,42 @@
-type heroi = {
-    name: string;
-    vulgo: string;
-};
+// type heroi = {
+//     name: string;
+//     vulgo: string;
+// };
 
-function printaObjeto(pessoa: heroi){
-    console.log(pessoa)
-}
+// function printaObjeto(pessoa: heroi){
+//     console.log(pessoa)
+// }
 
-console.log(printaObjeto({
-    name: "bruce wayner",
-    vulgo: "Batman",
-}))
+// console.log(printaObjeto({
+//     name: "bruce wayner",
+//     vulgo: "Batman",
+// }))
+
+let ligado: boolean = false;
+let nome: string = "Ariel";
+let idade: number = 30;
+let altura: number = 1.90;
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+let tantoFaz: any = "1";
+let retorno:void
+
+//objeto
+let produto:object = { 
+    name: "Ariel",
+    cidade: "SP",
+    idade: 30
+ };
+
+ type ProdutoLoja = {
+    nome: string;
+    preco: number
+    unidades:number;
+ }
+
+ let meuProduto: ProdutoLoja = {
+    nome: "Tenis",
+    preco: 100,
+    unidades:10,
+ }
